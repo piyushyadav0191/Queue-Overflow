02812171cd0dc1e321fc96287a3b92f9b38ec3cb
Piyush Yadav <piyushyadav0191@gmail.com>
2023-10-26 09:11:53 +0530

    shadcn added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
     container: {
       center: true,
@@ -16,53 +17,53 @@ const config: Config = {
     },
     extend: {
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          500: "#ff7000",
-          100: "#fff136",
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        dark: {
-          100: "#000000",
-          200: "#0f1117",
-          300: "#151821",
-          400: "#212734",
-          500: "#101012",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        light: {
-          900: "#ffffff",
-          800: "#f4f6f8",
-          850: "#fdfdfd",
-          700: "#dce3f1",
-          500: "#7b8ec8",
-          400: "#858ead",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        "accent-blue": "#1da1f2",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      fontFamily: {
-        inter: ["var(--font-inter)"],
-        spaceGortesk: ["var(--font-spaceGrotesk)"],
-      },
-      boxShadow: {
-        "light-100":
-          "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02) 0px 2px 4px 0px rgba(184, 184, 184, 0.03) ",
-        "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10) ",
-        "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10) ",
-        "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
-      },
-      backgroundImage: {
-        "auth-dark": "url('/assets/images/auth-dark.png",
-        "auth-light": "url('/assets/images/auth-light.png",
-      },
-      screens: {
-        xs: "420px",
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
+          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          to: { height: 0 },
         },
       },
       animation: {
@@ -71,6 +72,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+}
