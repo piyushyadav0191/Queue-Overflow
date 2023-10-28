ceb307041bc20cf775a9b5849a48c759ed6e917d
Piyush Yadav <piyushyadav0191@gmail.com>
2023-10-28 16:12:28 +0530

    ask a question

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import * as z from "zod";
+import { useRef, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
@@ -16,8 +18,15 @@ import { Input } from "@/components/ui/input";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { QuestionFormSchema } from "@/lib/validations";
+import { Badge } from "../ui/badge";
+import Image from "next/image";
+
+const type: any = "create";
 
 const Question = () => {
+  const editorRef = useRef(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const form = useForm<z.infer<typeof QuestionFormSchema>>({
     resolver: zodResolver(QuestionFormSchema),
     defaultValues: {
@@ -28,29 +37,165 @@ const Question = () => {
   });
 
   function onSubmit(values: z.infer<typeof QuestionFormSchema>) {
-    console.log(values);
+    setIsSubmitting(true);
+    try {
+    } catch (error) {
+    } finally {
+      setIsSubmitting(false);
+    }
   }
+
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag length should be less than 15 characters",
+          });
+        }
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const tags = field.value.filter((t: string) => t !== tag);
+    form.setValue("tags", tags);
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full flex-col gap-10"
+      >
         <FormField
           control={form.control}
           name="title"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="font-semibold">
+                Question Title <span className="text-primary-500">*</span>{" "}
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  placeholder="add title..."
+                  {...field}
+                  className="font-normal  min-h-[56px] border"
+                />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
+              <FormDescription className="mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another
+                person
               </FormDescription>
-              <FormMessage />
+              <FormMessage className="text-red-500" />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name="explanation"
+          render={({ field }) => (
+            <FormItem className="flex w-full gap-3 flex-col">
+              <FormLabel className="font-semibold">
+                Detailed explanation of your question{" "}
+                <span className="text-primary-500">*</span>{" "}
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINYCLOUD_API_KEY}
+                  //@ts-ignore
+                  onInit={(evt, editor) => (editorRef.current = editor)}
+                  initialValue="<p>This is the initial content of the editor.</p>"
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins:
+                      "  mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+                    toolbar:
+                      "undo redo | codesample |  blocks fontfamily fontsize | bold italic underline strikethrough | link  mergetags | align lineheight |  | checklist numlist bullist indent outdent | emoticons charmap ",
+                    content_style: "body { font-family:Inter; font-size:16px }",
+                  }}
+                />
+              </FormControl>
+              <FormDescription className="mt-2.5 text-light-500">
+                The better you explain your question the better answers you will
+                get
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="font-semibold">
+                Tags <span className="text-primary-500">*</span>{" "}
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <>
+                  <Input
+                    placeholder="add tags.."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                    className="font-normal  min-h-[56px] border"
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2 5 gap-2 5">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() => handleTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image
+                            src="/images/x.svg"
+                            alt="close"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
+              </FormControl>
+              <FormDescription className="mt-2.5 text-light-500">
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="bg-primary-500"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <>{type === "edit" ? "Editing" : "Posting..."}</>
+          ) : (
+            <>{type === "edit" ? "Edit Question" : "Ask a Question"}</>
+          )}
+        </Button>
       </form>
     </Form>
   );
--- lib/validations.ts
+++ lib/validations.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 export const QuestionFormSchema = z.object({
   title: z.string().min(2).max(50),
-  explanation: z.string().min(20).max(500),
+  explanation: z.string().min(10).max(500),
   tags: z.array(z.string().min(1).max(50)),
 });
